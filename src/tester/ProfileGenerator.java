7e0fe3ffefc26ec0de42b40c19a88ee4aafaa3c6
nathan <nathan@nathan-mint>
2014-11-19 19:06:37 -0600

    Added Profile Generator.

---
+++ src/tester/ProfileGenerator.java
@@ -0,0 +1,131 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Nathan Templon.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package tester;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Nathan Templon
+ */
+public class ProfileGenerator {
+
+    public static void main(String args[]) {
+        String bodyFile = "/home/nathan/body1.dat";
+        writeBodyProfile(bodyFile);
+    }
+
+    private static void writeBodyProfile(String filePath) {
+        final double bodyLength = 3.0;
+        final double bodyDiameter = 4.0 / 12.0;
+        final double taperLength = 6.0 / 12.0;
+        final double taperFinalDiam = 2.5 / 12.0;
+        final int numberOfIncs = 20;
+
+        final double bodyRadius = bodyDiameter / 2.0;
+        final double taperFinalRadius = taperFinalDiam / 2.0;
+
+        StringBuilder content = new StringBuilder();
+
+        // Top profile
+        // Nose
+        double amountPerInc = (Math.PI / 2.0) / 20.0;
+        for (int inc = 0; inc < numberOfIncs; inc++) {
+            double angle = inc * amountPerInc;
+            double x = bodyRadius * Math.sin(angle);
+            double y = bodyRadius * (1.0 - Math.cos(angle));
+
+            content.append((float) x).append(" ").append((float) y).append(
+                    System.lineSeparator());
+        }
+
+        // Body
+        amountPerInc = (bodyLength - taperLength - bodyRadius) / numberOfIncs;
+        for (int inc = 0; inc < numberOfIncs; inc++) {
+            double xPos = bodyRadius + amountPerInc * inc;
+            content.append((float) xPos).append(" ").append((float) bodyRadius).append(
+                    System.lineSeparator());
+        }
+
+        // Taper
+        amountPerInc = taperLength / numberOfIncs;
+        for (int inc = 0; inc < numberOfIncs; inc++) {
+            double xPos = (bodyLength - taperLength) + inc * amountPerInc;
+            double y = bodyRadius - (((xPos - (bodyLength - taperLength)) / taperLength) * (bodyRadius - taperFinalRadius));
+            content.append((float) xPos).append(" ").append((float) y).append(
+                    System.lineSeparator());
+        }
+        content.append((float) bodyLength).append(" ").append(
+                (float) taperFinalRadius).append(System.lineSeparator());
+
+        // Bottom profile
+        // Nose
+        amountPerInc = (Math.PI / 2.0) / 20.0;
+        for (int inc = 0; inc < numberOfIncs; inc++) {
+            double angle = inc * amountPerInc;
+            double x = bodyRadius * Math.sin(angle);
+            double y = -1 * (bodyRadius * (1.0 - Math.cos(angle)));
+
+            content.append((float) x).append(" ").append((float) y).append(
+                    System.lineSeparator());
+        }
+
+        // Body
+        amountPerInc = (bodyLength - taperLength - bodyRadius) / numberOfIncs;
+        for (int inc = 0; inc < numberOfIncs; inc++) {
+            double xPos = bodyRadius + amountPerInc * inc;
+            content.append((float) xPos).append(" ").append(
+                    (float) (-1.0 * bodyRadius)).append(
+                            System.lineSeparator());
+        }
+
+        // Taper
+        amountPerInc = taperLength / numberOfIncs;
+        for (int inc = 0; inc < numberOfIncs; inc++) {
+            double xPos = (bodyLength - taperLength) + inc * amountPerInc;
+            double y = -1.0 * (bodyRadius - (((xPos - (bodyLength - taperLength)) / taperLength) * (bodyRadius - taperFinalRadius)));
+            content.append((float) xPos).append(" ").append((float) y).append(
+                    System.lineSeparator());
+        }
+        content.append((float) bodyLength).append(" ").append(
+                (float) taperFinalRadius).append(System.lineSeparator());
+
+        // Output
+        File output = new File(filePath);
+        try (FileWriter fw = new FileWriter(output);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter pw = new PrintWriter(bw)) {
+
+            pw.println(content.toString());
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
